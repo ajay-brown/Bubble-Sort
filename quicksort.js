64a1558bc4f1742d2d212b3e542090dbdbb54182
Audrey <audreybrown94@gmail.com>
2018-01-25 17:50:02 -1000

    how to include equal values?

--- quicksort.js
+++ quicksort.js
@@ -1,22 +1,20 @@
 //using recursion + 3 variables 
-let newArr= [];
 function quickSort(arr) {
 let leftArr =[];
 let rightArr = [];
 let equalArr = [];
-let pivot = (arr.length /2) || 0;
-    if (arr.length < 2){
+let pivot =  arr[0];
+    if (arr.length === 0){
         return arr;
     }
-    for (var i=0;i<arr.length;i++){
-        if(arr[i]>arr[pivot]) { 
-            rightArr.push(arr[i]);
-        } else if (arr[i] < data[pivot]) {
+    for (var i=1;i<arr.length;i++){
+        if(arr[i]<arr[pivot]) { 
             leftArr.push(arr[i]);
+        } else if (arr[i] > arr[pivot]) {
+            rightArr.push(arr[i]);
         } else {
             equalArr.push(arr[i]);
         }
     }
-    newArr.push(leftArr, equalArr, rightArr);
-  //  quickSort(newArr);
+    return quickSort(leftArr).concat(pivot,equalArr, quickSort(rightArr));
 }
